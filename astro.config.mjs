cf865a0359710b19e8b5ce605dd5387561900b43
Khadim <khadimfall0602@gmail.com>
2024-01-17 23:35:44 +0100

    pages settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://mono424.github.io',
+  base: '/portfolio3',
 });
